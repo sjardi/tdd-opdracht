e6ddce27f45cdf17194cdcef160259f2002db16d
sjard <sjard@msn.com>
2019-05-05 01:22:00 +0200

    Test class en methods package private gemaakt

--- src/test/java/nl/hu/taxcalculator/AOWQualifierTest.java
+++ src/test/java/nl/hu/taxcalculator/AOWQualifierTest.java
@@ -1,16 +1,11 @@
 package nl.hu.taxcalculator;
 
 import static java.time.LocalDate.of;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import java.util.Date;
-
-public class AOWQualifierTest {
+class AOWQualifierTest {
 	
 	private AOWQualifier aowQualifier;
 	
@@ -19,24 +14,36 @@ public class AOWQualifierTest {
 //		aowQualifier = new AOWQualifier(of(2016,1,1));
 //	}
 
+    @Disabled("This test has been disabled.")
+    @Test
+    void disabledTest(){
+        assertEquals(1, 1, "Is '1' gelijk aan '1' ");
+    }
+
 	@Test
 	@DisplayName("This should always be true")
-	public void testTrue() {
+    private void testTrue() {
 		assertTrue(true);
 	}
 
 	@Test
 	@DisplayName("1950-12-1 geboren voor de datum van 2016-1-1")
-	public void shouldNotQualify(){
+    void shouldNotQualify(){
         aowQualifier = new AOWQualifier(of(2016,1,1));
 		assertFalse(aowQualifier.doesDateQualify(of(1950,12,1)));
 	}
 
     @Test
     @DisplayName("1950-12-1 geboren voor de datum van 2017-1-1")
-    public void shouldQualify(){
+    void shouldQualify(){
         aowQualifier = new AOWQualifier(of(2017,1,1));
         assertTrue(aowQualifier.doesDateQualify(of(1950,12,1)));
+    }
+
+
+    @AfterAll
+    static void afterAll(){
+	    System.out.println("All test are Done.");
     }
 
 
